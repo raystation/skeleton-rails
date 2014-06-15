f743e8f91182057f30e23bf0c4fd9f8a8ac4f913
Ray <rayuen@gmail.com>
2014-06-14 21:25:25 -0700

    got different pages set up

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,16 @@
 class PagesController < ApplicationController
 
   def home
+  	@title = "Home"
   end
 
   def contact
+  	@title = "Contact"
+  	@text = "January Jones"
+  end
+
+  def about
+  	@title = "About"
   end
 
 end
--- app/views/pages/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @page, :id, :home, :about, :created_at, :updated_at
